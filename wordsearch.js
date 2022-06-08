541fd6a9e67fc900d6f220a08c8ae95552a2f6ef
Jesse Shen <jly.shen@gmail.com>
2022-06-08 22:23:31 +0000

    modified and completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,7 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters === undefined || letters.length < 1) {
+        return false;
+    }
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = [];
+
+    for (let col = 0; col < letters[0].length; col++) {
+        let str = '';
+        for (let row = 0; row < letters.length; row++) {
+
+            //for backwards
+            // for (let row = letters.length; row >= 0; row--) {
+
+            // arr[0][0] - arr[1][1] - arr[2][2]...
+            // arr[1][0] - arr[2][1] - arr[3][2]... 
+
+            str += letters[row][col];
+        }
+        verticalJoin.push(str);
+    }
+
+    if (wordChecker(horizontalJoin, word) || wordChecker(verticalJoin, word)) {
+        return true;
+    };
+
+    return false
+}
+
+const wordChecker = function (array, word) {
+    for (const string of array) {
+        if (string.includes(word)) return true;
     }
 }
 
